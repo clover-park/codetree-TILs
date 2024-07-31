4e9d37a64a780d3304bd3e2424290ad27d8d0274
clover-park <79325546+clover-park@users.noreply.github.com>
2024-07-31 12:54:06 +0900

    Title: 100 도달하기, Time: 123ms, Memory: 73MB, Status: Passed - CodeTree

--- 240731/100 도달하기/reach-100.py
+++ 240731/100 도달하기/reach-100.py
@@ -1,13 +1,13 @@
 # arr = [0]*11
 # = arr = [0,0,0,0,0,0,0,0,0,0,0]
-arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0]
 num = int(input())
 arr[0] = 1
 arr[1] = num
 print(arr[0], arr[1], end=' ')
 
 # 3번째 항부터 10번째 항까지 추가
-for i in range(2, 11): 
+for i in range(2, 12): 
     arr[i] = arr[i - 1] + arr[i - 2]
     print(arr[i], end = ' ')
     if arr[i] > 100:
