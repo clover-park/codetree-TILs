6cde8c0a192ad69e4dfbaf862627359185715c88
clover-park <79325546+clover-park@users.noreply.github.com>
2024-07-31 14:45:21 +0900

    Title: 가장 큰 수 고르기, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240731/가장 큰 수 고르기/picking-biggest-number.py
@@ -0,0 +1,12 @@
+# 초기값 세팅이 중요하다.
+
+arr = list(map(int, input().split()))
+
+# 초기값 세팅하기
+max_val = 0
+# max_val = -sys.maxsize
+for i in arr:
+    if i > max_val:
+        max_val = i
+
+print(max_val)
