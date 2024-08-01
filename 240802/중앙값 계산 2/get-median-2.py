d05b7136c5ced297835cb5168269c6557c096c7d
clover-park <79325546+clover-park@users.noreply.github.com>
2024-08-02 00:52:09 +0900

    Title: 중앙값 계산 2, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240802/중앙값 계산 2/get-median-2.py
+++ 240802/중앙값 계산 2/get-median-2.py
@@ -1,10 +1,10 @@
 n = int(input())
 
 arr = list(map(int, input().split()))
-arr.sort()
+sort_arr = []
 
 for i in range(n):
-    if i == 0:
-        print(arr[0], end=' ')
-    elif i % 2 != 0:
-        print(arr[(i+1)//2], end=' ')
+    sort_arr.append(arr[i])
+    if (i+1) % 2 != 0:
+        sort_arr.sort()
+        print(sort_arr[i//2], end=' ')
