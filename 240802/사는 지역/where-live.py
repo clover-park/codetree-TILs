53bd64b467068ec0afa762a5fa57d98540c48902
clover-park <79325546+clover-park@users.noreply.github.com>
2024-08-02 09:17:35 +0900

    Title: 사는 지역, Time: 143ms, Memory: 24MB, Status: Passed - CodeTree

--- 240802/사는 지역/where-live.py
+++ 240802/사는 지역/where-live.py
@@ -1,3 +1,7 @@
+'''
+enumerate 함수는 i, elem 값을 순차적으로 반환해줍니다.
+(순서가 바뀌면 절대 안됩니다)
+'''
 class Info:
     def __init__(self, name, address, city):
         self.name = name
@@ -10,9 +14,9 @@ people = [Info(name, address, city) for name, address, city in arr]
 
 index = 0
 
-for person in people:
+for i, person in enumerate(people):
     if person.name > people[index].name:
-        index += 1
+        index = i
 
 print(f'name {people[index].name}')
 print(f'addr {people[index].address}')
