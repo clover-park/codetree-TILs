f7439bb73694dc609669dd4541251b6d36d50ca2
clover-park <79325546+clover-park@users.noreply.github.com>
2024-08-04 22:50:02 +0900

    Title: 십진수와 이진수 2, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240804/십진수와 이진수 2/decimal-and-binary-number-2.py
@@ -0,0 +1,22 @@
+n = list(input())
+
+binary = list(map(int, n))
+num = 0
+
+for i in range(len(binary)):
+    num = num * 2 + binary[i]
+
+num *= 17
+
+digits = []
+
+while True:
+    if num < 2:
+        digits.append(num)
+        break
+    
+    digits.append(num % 2)
+    num //= 2
+
+for digit in digits[::-1]:
+    print(digit, end='')
