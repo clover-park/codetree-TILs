6b6da4a19b6632310eaee1b6f1d562a95d1d9ebb
clover-park <79325546+clover-park@users.noreply.github.com>
2024-08-04 22:41:04 +0900

    Title: 여러가지 진수변환, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240804/여러가지 진수변환/various-numeral-system-transformations.py
+++ 240804/여러가지 진수변환/various-numeral-system-transformations.py
@@ -2,11 +2,13 @@ n, b = tuple(map(int,input().split()))
 
 digits = []
 
-while n > 1:
+while True:
+    if n < b:
+        digits.append(n)
+        break
+    
     digits.append(n % b)
     n //= b
-
-digits.append(n)
 
 for digit in digits[::-1]:
     print(digit, end='')
