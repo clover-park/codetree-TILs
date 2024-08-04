71834f84ad2efcc2b4fa7af2706dbe795f1baace
clover-park <79325546+clover-park@users.noreply.github.com>
2024-08-04 20:55:47 +0900

    Title: DateTime to DateTime, Time: 5083ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

--- 240804/DateTime to DateTime/datetime-to-datetime.py
+++ 240804/DateTime to DateTime/datetime-to-datetime.py
@@ -3,21 +3,21 @@ a, b, c = tuple(map(int, input().split()))
 date, hour, mins = 11, 11, 11
 elapsed_time = 0
 
-while True:
-    if date == a and hour == b and mins == c:
-        break
-    elapsed_time += 1
-    mins += 1
-
-    if mins == 60:
-        hour += 1
-        mins = 0
-
-    if hour == 24:
-        date += 1
-        hour = 0
-
-if a < 11 and b < 11 and c < 11:
+if a < 11:
     print(-1)
 
-print(elapsed_time)
+else:
+    while True:
+        if date == a and hour == b and mins == c:
+            break
+        elapsed_time += 1
+        mins += 1
+
+        if mins == 60:
+            hour += 1
+            mins = 0
+
+        if hour == 24:
+            date += 1
+            hour = 0
+    print(elapsed_time)
